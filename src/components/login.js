a12ffabe68cdf8b6466ad16888a8b6bb6d64eef3
Lou BEGE <wolfla@hotmail.fr>
2021-02-09 11:32:26 +0100

    Style: Add css for login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import '../css/login.css';
+
 
 class Login extends React.Component {
+    
     render() {
         return (
-            <div>
+            <div className="loginContent">
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>Username :</Form.Label>
+                        <Form.Control type="text" placeholder="Enter username" />
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Label>Password :</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
