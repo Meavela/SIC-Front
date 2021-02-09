bcdcb828d79e8d9fdc7a4c66f7779430810c38f7
Lou BEGE <wolfla@hotmail.fr>
2021-02-09 10:51:09 +0100

    Feat: Add bootstrap

--- src/components/login.js
+++ src/components/login.js
@@ -3,7 +3,19 @@ import React from 'react';
 class Login extends React.Component{
     render(){
         return (
-            <h1>Hello</h1>
+            <div>
+                <div>
+                    Username :
+                    <input type="text"></input>
+                </div>
+                <div>
+                    Password :
+                    <input type="password"></input>
+                </div>
+                <div>
+                    <button type="button" className="btn btn-primary">Login</button>
+                </div>
+            </div>
         )
     }
 }
