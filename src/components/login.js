195ab08c3fd1d02a8102cfb572c7a403bb256dc4
Lou BEGE <wolfla@hotmail.fr>
2021-02-09 11:57:34 +0100

    Feat: Add process of login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../css/login.css';
-
+import  { Redirect } from 'react-router-dom'
 
 class Login extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = { username: '', password: '', isRedirect: false };
+    }
+
+    changeUsername = (event) => {
+        console.log(event.target.value);
+        this.setState({username: event.target.value});
+    }
+
+    changePassword = (event) => {
+        console.log(event.target.value);
+        this.setState({password: event.target.value});
+    }
+
+    submitLogin = () => {
+        console.log(this.state)
+        if (this.state.username == 'admin' && this.state.password == '123456') {
+            console.log("redirect")
+        }
+    }
+
+
     render() {
         return (
             <div className="loginContent">
                 <Form>
                     <Form.Group controlId="formBasicUsername">
                         <Form.Label>Username :</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control type="text" placeholder="Enter username" onChange={this.changeUsername}/>
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password :</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Password" />
+                        <Form.Control type="password" placeholder="Enter Password" onChange={this.changePassword}/>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" onClick={this.submitLogin}>
                         Submit
-                </Button>
+                    </Button>
                 </Form>
             </div>
         )
