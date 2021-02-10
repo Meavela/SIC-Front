f187ce482a27baaedbf868e05d11a110a33baeda
Lou BEGE <wolfla@hotmail.fr>
2021-02-10 11:16:54 +0100

    Feat: Add routing for admin

---
+++ src/components/admin-votes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AdminVotes extends React.Component {
+    render(){
+        return (
+            <>
+                <h1>Admin Votes Page</h1>
+            </>
+        )
+    }
+}
+
+export default AdminVotes
--- src/components/router.js
+++ src/components/router.js
@@ -4,6 +4,9 @@ import NavbarComponent from "./navbar-component";
 import Result from './result';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Login from './login';
+import Admin from './admin';
+import AdminUsers from './admin-users';
+import AdminVotes from './admin-votes';
 
 const RouterNav = () => {
 
@@ -20,6 +23,9 @@ const RouterNav = () => {
                 <div>
                     <Route exact path="/" component={Result} />
                     <Route exact path="/login" component={() => <Login username={username} setUsername={setUsername} />}/>
+                    <Route exact path="/admin" component={Admin}/>
+                    <Route exact path="/admin/users" component={AdminUsers}/>
+                    <Route exact path="/admin/votes" component={AdminVotes}/>
                 </div>
             </Suspense>
         </Router>
