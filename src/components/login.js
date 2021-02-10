6c447df9ff2354d5a8e3dda9dc5745237081cb1b
Lou BEGE <wolfla@hotmail.fr>
2021-02-10 09:31:22 +0100

    Refactor: remove useless

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../css/login.css';
-import  { Redirect, useHistory } from 'react-router-dom';
+import  { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
     constructor(props) {
