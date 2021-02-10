6b6a293673d181fd0cbc957669e062fa90251932
Lou BEGE <wolfla@hotmail.fr>
2021-02-10 09:18:51 +0100

    Feat: Redirect on login page works

--- src/components/login.js
+++ src/components/login.js
@@ -6,10 +6,8 @@ import  { Redirect, useHistory } from 'react-router-dom';
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { username: '', password: '', error: '' };
+        this.state = { username: '', password: '', error: '', redirect: false };
     }
-
-    
 
     changeUsername = (event) => {
         console.log(event.target.value);
@@ -21,43 +19,48 @@ class Login extends React.Component {
         this.setState({password: event.target.value});
     }
 
-    submitLogin = (event) => {
+    submitLogin = () => {
         console.log(this.state)
         if (this.state.username == 'admin' && this.state.password == '123456') {
             console.log("redirect")
-            this.setState({error: ''});
+            this.setState({error: '', redirect: true});
         }else {
-            this.setState({error: 'Username or password are not correct, please try again'});
+            this.setState({error: 'Username or password are not correct, please try again', redirect: false});
         }
     }
 
 
     render() {
-        return (
-            <div className="loginContent">
-                <Form>
-                    <Form.Group controlId="formBasicUsername">
-                        <Form.Label>Username :</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" onChange={this.changeUsername}/>
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password :</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Password" onChange={this.changePassword}/>
-                    </Form.Group>
-                    
-                    <Form.Group>
-                        <Form.Label className="errorContent">{this.state.error}</Form.Label>
-                    </Form.Group>
-
-                    <Button variant="primary" onClick={this.submitLogin}>
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        )
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        }else{
+            return (
+                <div className="loginContent">
+                    <Form>
+                        <Form.Group controlId="formBasicUsername">
+                            <Form.Label>Username :</Form.Label>
+                            <Form.Control type="text" placeholder="Enter username" onChange={this.changeUsername}/>
+                            <Form.Text className="text-muted">
+                            </Form.Text>
+                        </Form.Group>
+    
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password :</Form.Label>
+                            <Form.Control type="password" placeholder="Enter Password" onChange={this.changePassword}/>
+                        </Form.Group>
+                        
+                        <Form.Group>
+                            <Form.Label className="errorContent">{this.state.error}</Form.Label>
+                        </Form.Group>
+    
+                        <Button variant="primary" onClick={this.submitLogin}>
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            )
+        }
+        
     }
 }
 
