9aa16dfffcf4f61652807c59157c6ff64c5551a4
Marc-Antoine <marco38000@gmail.com>
2021-02-25 11:51:43 +0100

    Test : correct all bugs on css

--- src/components/admin-users.js
+++ src/components/admin-users.js
@@ -1,23 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Form, Button,Table } from 'react-bootstrap';
-import '../css/admin-users.css';
+import { Form, Button, Table } from 'react-bootstrap';
 import axios from 'axios';
+
+const createContent = {
+    marginTop: '10%'
+}
+
+const contentForm = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%'
+}
+
+const errorContent = {
+    color: 'red',
+    fontWeight: 'bold',
+    textAlign: 'center'
+}
+
+const successContent = {
+    color: 'green',
+    fontWeight: 'bold',
+    textAlign: 'center'
+}
+
+const submitButton = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '20%'
+}
+
+const buttonWidth = {
+    width: '120%',
+}
+
+const title = {
+    marginTop: '5%',
+    textAlign: 'center'
+}
+
+const contentTable = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '2%',
+    textAlign: 'center',
+    width: '80%'
+}
 
 class AdminUsers extends React.Component {
     constructor(props) {
         super(props);
         console.log(this.props)
-        this.state = { 
-            listUsers: [], 
-            username: '', 
-            password: '', 
+        this.state = {
+            listUsers: [],
+            username: '',
+            password: '',
             error: '',
-            success: ''};
+            success: ''
+        };
     }
 
     componentDidUpdate() {
-        if(this.state.redirect) {
+        if (this.state.redirect) {
             this.props.setUsername(this.state.username);
         }
     }
@@ -38,15 +83,15 @@ class AdminUsers extends React.Component {
 
     changeUsername = (event) => {
         console.log(event.target.value);
-        this.setState({username: event.target.value});
+        this.setState({ username: event.target.value });
     }
 
     changePassword = (event) => {
         console.log(event.target.value);
-        this.setState({password: event.target.value});
+        this.setState({ password: event.target.value });
     }
 
-    async isExistUser(json){
+    async isExistUser(json) {
         console.log(json)
         let login = await axios.post('http://localhost:3005/login', json)
         console.log(login)
@@ -58,7 +103,7 @@ class AdminUsers extends React.Component {
         }
     }
 
-    async addUser(json){
+    async addUser(json) {
         let isCreated = await axios.post('http://localhost:3005/users/add', json)
         console.log(isCreated)
         if (isCreated.data.status == "OK") {
@@ -71,59 +116,59 @@ class AdminUsers extends React.Component {
 
     submitCreate = async () => {
         console.log(this.state)
-        var json = {"username":this.state.username, "password": this.state.password};
+        var json = { "username": this.state.username, "password": this.state.password };
         var isExist = await this.isExistUser(json);
         if (isExist) {
             console.log("User existe")
-            this.setState({error: 'Username already exists'});
-        }else{
+            this.setState({ error: 'Username already exists' });
+        } else {
             console.log("User n'existe pas")
             if (await this.addUser(json)) {
-                this.setState({success: 'Your user has been created'});
+                this.setState({ success: 'Your user has been created' });
                 // ajout à la liste
-            }else{
-                this.setState({error: 'Username already exists'});
+            } else {
+                this.setState({ error: 'Username already exists' });
             }
         }
     }
 
-    render(){
+    render() {
         return (
             <>
                 <Link to="/admin" >⬅ Back</Link>
-                <div className="contentForm">
-                    <h1 className="title">Admin Users Page - Users</h1>
-                    <div className="createContent">
+                <div className={contentForm}>
+                    <h1 className={title}>Admin Users Page - Users</h1>
+                    <div className={createContent}>
                         <Form>
                             <Form.Group controlId="formBasicUsername">
                                 <Form.Label>Username :</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" onChange={this.changeUsername}/>
+                                <Form.Control type="text" placeholder="Enter username" onChange={this.changeUsername} />
                                 <Form.Text className="text-muted">
                                 </Form.Text>
                             </Form.Group>
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password :</Form.Label>
-                                <Form.Control type="password" placeholder="Enter Password" onChange={this.changePassword}/>
+                                <Form.Control type="password" placeholder="Enter Password" onChange={this.changePassword} />
                             </Form.Group>
 
-                            <Form.Group className="errorContent">
+                            <Form.Group className={errorContent}>
                                 <Form.Label >{this.state.error}</Form.Label>
                             </Form.Group>
 
-                            <Form.Group className="submitButton">
-                                <Button variant="primary" onClick={this.submitCreate}>
+                            <Form.Group className={submitButton}>
+                                <Button className={buttonWidth} variant="primary" onClick={this.submitCreate}>
                                     Create
                                 </Button>
                             </Form.Group>
-                            
 
-                            <Form.Group className="successContent">
+
+                            <Form.Group className={successContent}>
                                 <Form.Label >{this.state.success}</Form.Label>
                             </Form.Group>
                         </Form>
-                    </div>                    
+                    </div>
                 </div>
-                <div className="contentTable">
+                <div className={contentTable}>
                     <Table striped bordered hover variant="blue">
                         <thead>
                             <tr>
--- src/components/admin.js
+++ src/components/admin.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import '../css/admin.css';
+
+const styleContainer = {
+    marginTop: '10%'
+}
+
+const styleCard = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '80%'
+}
 
 class Admin extends React.Component {
-    render(){
+    render() {
         return (
             <>
-                <Container className="styleContainer">
+                <Container className={styleContainer}>
                     <Row>
                         <Col>
-                            <Card className="styleCard">
+                            <Card className={styleCard}>
                                 <Card.Body>
                                     <Card.Title>Admin Users</Card.Title>
                                     <Card.Subtitle className="mb-2 text-muted">Create new users</Card.Subtitle>
                                     <Card.Text>
-                                    This admin pages allows to create new users account for the application.
-                                    Only an administrator can access to this page.
+                                        This admin pages allows to create new users account for the application.
+                                        Only an administrator can access to this page.
                                     </Card.Text>
                                     <Link to="/admin/users" >Access here</Link>
                                 </Card.Body>
@@ -28,9 +37,9 @@ class Admin extends React.Component {
                                     <Card.Title>Admin Votes</Card.Title>
                                     <Card.Subtitle className="mb-2 text-muted">Create/Delete choices votes</Card.Subtitle>
                                     <Card.Text>
-                                    This admin pages allows to create or delete choices for votes.
-                                    We can't delete user vote, only the choices.
-                                    Only an administrator can access to this page.
+                                        This admin pages allows to create or delete choices for votes.
+                                        We can't delete user vote, only the choices.
+                                        Only an administrator can access to this page.
                                     </Card.Text>
                                     <Link to="/admin/votes" >Access here</Link>
                                 </Card.Body>
